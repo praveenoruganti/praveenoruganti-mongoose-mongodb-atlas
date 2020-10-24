5950b78a62a15daa8290b3dcabdb24a5e178b77d
Praveen Oruganti <praveenorugantitech@gmail.com>
2020-10-24 13:18:23 +0530

    updated

--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const foodModel = require('../models/food');
 const app = express();
 
-app.get('/foods', async (req, res) => {
+app.get('/foods', (req, res) => {
   const foods = await foodModel.find({});
 
   try {
@@ -12,7 +12,7 @@ app.get('/foods', async (req, res) => {
   }
 })
 
-app.post('/food', async (req, res) => {
+app.post('/food', (req, res) => {
   const food = new foodModel(req.body);
 
   try {
@@ -23,7 +23,7 @@ app.post('/food', async (req, res) => {
   }
 })
 
-app.delete('/food/:id', async (req, res) => {
+app.delete('/food/:id', (req, res) => {
   try {
     const food = await foodModel.findByIdAndDelete(req.params.id)
 
@@ -34,7 +34,7 @@ app.delete('/food/:id', async (req, res) => {
   }
 })
 
-app.patch('/food/:id', async (req, res) => {
+app.patch('/food/:id', (req, res) => {
   try {
     await foodModel.findByIdAndUpdate(req.params.id, req.body)
     await foodModel.save()
