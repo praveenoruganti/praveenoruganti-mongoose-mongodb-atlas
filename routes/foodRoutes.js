8f644b26ee184592457eb5b2da07cde971879a6d
Praveen Oruganti <praveenorugantitech@gmail.com>
2020-10-24 13:35:44 +0530

    updated

--- routes/foodRoutes.js
+++ routes/foodRoutes.js
@@ -2,46 +2,47 @@ const express = require('express');
 const foodModel = require('../models/food');
 const app = express();
 
-app.get('/foods', (req, res) => {
-  const foods = foodModel.find({});
+app.get('/foods', async (req, res) => {
+  const foods = await foodModel.find({});
 
   try {
     res.send(foods);
   } catch (err) {
     res.status(500).send(err);
   }
-})
+});
 
-app.post('/food', (req, res) => {
+app.post('/food', async (req, res) => {
   const food = new foodModel(req.body);
 
   try {
-    food.save();
+    await food.save();
     res.send(food);
   } catch (err) {
     res.status(500).send(err);
   }
-})
+});
 
-app.delete('/food/:id', (req, res) => {
+app.delete('/food/:id', async (req, res) => {
   try {
-    const food = foodModel.findByIdAndDelete(req.params.id)
+    const food = await foodModel.findByIdAndDelete(req.params.id)
 
     if (!food) res.status(404).send("No item found")
     res.status(200).send()
   } catch (err) {
     res.status(500).send(err)
   }
-})
+});
 
-app.patch('/food/:id', (req, res) => {
+app.patch('/food/:id', async (req, res) => {
   try {
-    foodModel.findByIdAndUpdate(req.params.id, req.body)
-    foodModel.save()
+    await foodModel.findByIdAndUpdate(req.params.id, req.body)
+    await foodModel.save()
     res.send(food)
   } catch (err) {
     res.status(500).send(err)
   }
-})
+});
+
 
 module.exports = app
